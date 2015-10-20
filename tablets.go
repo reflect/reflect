27f313d47dc45909ce65d9021c4148c797453b74
Brad Heller <brad.heller@gmail.com>
2015-10-20 11:09:10 -0700

    Add two new operations to keyspaces.

--- tablets.go
+++ tablets.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+const (
+	CriteriaHeaderName = "X-Criteria"
+	UpsertHeaderName   = "X-Insert-Missing"
+)
+
 func tabletPath(keyspace, key string) string {
 	return fmt.Sprintf("/%s/keyspaces/%s/tablets/%s", defaultAPIVersion, keyspace, key)
 }
@@ -23,6 +28,32 @@ func (client *Client) Append(keyspace, key string, recs interface{}) error {
 // The supplied records (recs) can be either a map or a slice of maps.
 func (client *Client) Replace(keyspace, key string, body interface{}) error {
 	req := client.newRequest("POST", tabletPath(keyspace, key), dump(body))
+	_, err := client.do(req, http.StatusAccepted)
+	return err
+}
+
+// Update a set of records in a tablet using criteria to match records between
+// the supplied records and the records that exist in the tablet. Any supplied
+// records that don't match will be dropped.
+//
+// The supplied records (recs) can be either a map or a slice of maps.
+func (client *Client) Patch(keyspace, key string, criteria []string, body interface{}) error {
+	req := client.newRequest("PATCH", tabletPath(keyspace, key), dump(body))
+	req.Header.Add(CriteriaHeaderName, strings.Join(criteria, ", "))
+	_, err := client.do(req, http.StatusAccepted)
+	return err
+}
+
+// Update or insert a set of records in a tablet using criteria to match
+// records between the supplied records and the records that exist in the
+// tablet. Any records that don't match will be appended.
+//
+// The supplied records (recs) can be either a map or a slice of maps.
+func (client *Client) Upsert(keyspace, key string, criteria []string, body interface{}) error {
+	req := client.newRequest("PATCH", tabletPath(keyspace, key), dump(body))
+	req.Header.Add(CriteriaHeaderName, strings.Join(criteria, ", "))
+	req.Header.Add(UpsertHeaderName, true)
+
 	_, err := client.do(req, http.StatusAccepted)
 	return err
 }
