4dd24e85aaa9a4f3a5441cfc0104470034962e9f
Brad Heller <brad.heller@gmail.com>
2016-05-04 20:58:44 -0700

    Typos abound.

--- utils.go
+++ utils.go
@@ -24,9 +24,9 @@ func logError(format string, args ...interface{}) {
 }
 
 // Given a secret key and a set of parameters, generates a new signed
-// authentication token for using when authenticing clients.
+// authentication token for using when authenticating clients.
 //
-// An example of how to generate an authentication token.
+// Here is an example of how to generate a signed authentication token.
 //
 //	params := []reflect.Parameter{
 //	  {
